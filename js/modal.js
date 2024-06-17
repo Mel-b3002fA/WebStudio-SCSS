4808115accc0f72da50a8dd296e3501cda17d1a4
Melaniia <raventa234@gmail.com>
2024-06-17 20:02:01 +0200

    push

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-open-modal]"),
+      closeModalBtn: document.querySelector("[data-close-modal]"),
+      modal: document.querySelector("[data-modal]"),
+      backdrop: document.querySelector("[data-backdrop]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.backdrop.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
